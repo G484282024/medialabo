051dcc1f60b4a3a07632058f7f914dd8b747447c
G484282024 <mailto:248428@st.takushoku-u.ac.jp>
2025-07-20 22:11:47 +0900

    変更しました

--- weather.js
+++ weather.js
@@ -60,22 +60,22 @@ document.getElementById("searchBtn").addEventListener("click", function (){
 // 課題5-1 の関数 printDom() はここに記述すること
 function printDom(data) {
 let da=[
-        {a:'都市名'+data.name},
-        {a:'経度'+(data.coord.lon)},
-        {a:'緯度'+data.coord.lat},
-        {a:'天気'+data.weather[0].description},
-         {a:'最高気温'+data.main.temp_max},
-        {a:'最低気温'+data.main.temp_min},
-        {a:'湿度'+data.main.humidity},
-        {a:'風速'+data.wind.speed},
-        {a:'風向'+data.wind.deg},
+        {a:'都市名:  '+data.name},
+        {a:'経度:   '+(data.coord.lon)+'°' },
+        {a:'緯度:  '+data.coord.lat+'°' },
+        {a:'天気:  '+data.weather[0].description},
+         {a:'最高気温:  '+data.main.temp_max+' ℃'},
+        {a:'最低気温:  '+data.main.temp_min+' ℃'},
+        {a:'湿度:  '+data.main.humidity+' %'},
+        {a:'風速:  '+data.wind.speed+' m/s'},
+        {a:'風向:  '+data.wind.deg+'°'},
       ];
 
     let di=document.querySelector('div#result'); 
     let une=document.createElement('ul');
     une.id='box';
     une.setAttribute('id','box');
-    di.insertAdjacentElement('afterend',u); 
+    di.insertAdjacentElement('beforeend',une); 
      
 
     for(let aun of da){
@@ -106,12 +106,25 @@ let To={360630:'Cairo カイロ （エジプト)',
 
 
 
-// 課題6-1 のイベントハンドラ sendRequest() の定義
+// 課題6-1 のイベントハンドラs sendRequest() の定義
 function sendRequest() {
   let url = "https://www.nishita-lab.org/web-contents/jsons/openweather/"
-  
+  let x=document.querySelector("#cityId");
+  let ku = x.value;
+  let id = cityMap[ku];
+  console.log(ku);  
+  console.log('入力された都市名:', ku);
+  console.log('対応する都市ID:', id);
+
+  if (!id) {
+    console.log('入力された都市名に対応する都市IDが見つかりません。正しい名前を入力してください。');
+    return;  
+  }
+
+  let url11 = url + id + '.json';
+
     // 通信開始
-    axios.get(url)
+    axios.get(url11)
         .then(showResult)   // 通信成功
         .catch(showError)   // 通信失敗
         .then(finish);      // 通信の最後の処理
@@ -121,6 +134,10 @@ function sendRequest() {
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
 let data = resp.data;
+u = document.querySelector('ul#box'); 
+if (u !== null) {
+  u.remove();
+}
 
     // data が文字列型なら，オブジェクトに変換する
     if (typeof data === 'string') {
@@ -130,16 +147,20 @@ let data = resp.data;
     // data をコンソールに出力
   
     console.log(data); 
+    // data.x を出力
+	console.log(data.x);
+
+  printDom(data);
 
   console.log("都市名: " + data.name);  
-  console.log ('経度'+(data.coord.lon));
-       console.log ('緯度'+data.coord.lat);
+  console.log ('経度'+(data.coord.lon)+'°');
+       console.log ('緯度'+data.coord.lat+'°');
     console.log("天気: " + data.weather[0].description);   
-     console.log ('最高気温'+data.main.temp_max);
-        console.log('最低気温'+data.main.temp_min);
+     console.log ('最高気温'+data.main.temp_max+' ℃');
+        console.log('最低気温'+data.main.temp_min+' ℃');
     console.log("湿度: " + data.main.humidity + " %");  
     console.log("風速: " + data.wind.speed + " m/s"); 
-        console.log('風向'+data.wind.deg);
+        console.log('風向'+data.wind.deg+'°');
 
    
 }
@@ -158,50 +179,3 @@ function finish() {
 // 以下はグルメのデータサンプル
 // 注意: 第5回までは以下を変更しないこと！
 // 注意2: 課題6-1 で以下をすべて削除すること
-/*let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
-*/
